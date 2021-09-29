e05f88125721fe90a9eaee0d00250d470d25f68b
0-Hoppi-0 <91344869+0-Hoppi-0@users.noreply.github.com>
2021-09-29 13:51:14 +0300

    Update prediction_of_furniture.py

--- prediction_of_furniture.py
+++ prediction_of_furniture.py
@@ -1,18 +1,15 @@
 import compress_fasttext
+
+
 small_model = compress_fasttext.models.CompressedFastTextKeyedVectors.load( 
    'https://github.com/avidale/compress-fasttext/releases/download/v0.0.1/ft_freqprune_100K_20K_pq_300.bin' ) 
-print(small_model['Абоба'])
-word = ['современный', 'актуальный', 'модный', 'сегодняшний', 'текущий', 'настоящий', 'крутой', 'новый', 'моментный', 'старый', 'древний', 'уютный', 'ностальгичный', 'ранний', 'давний', 'прошлый', 'предшествующий', 'классический', 'стандартный', 'нормальный', 'обычный', 'типичный', 'характерный', 'образцовый']
+
+
 word1 = ['современный', 'актуальный', 'модный', 'сегодняшний', 'текущий', 'настоящий', 'крутой', 'новый', 'моментный']
 word2 = ['старый', 'древний', 'уютный', 'ностальгичный', 'ранний', 'давний', 'прошлый', 'предшествующий']
 word3 = ['классический', 'стандартный', 'нормальный', 'обычный', 'типичный', 'характерный', 'образцовый']
-model = []
-for x in range(len(word1)):
-  model.append(1)
-for x in range(len(word2)):
-  model.append(2)
-for x in range(len(word3)):
-  model.append(3)
+
+
 def emb_mod(words):
   vecs = []
   s = []
@@ -26,17 +23,21 @@ def emb_mod(words):
       n += 1
     s.append(col / n)
   return s
+
+
 models = []
 models.append(emb_mod(word1))
 models.append(emb_mod(word2))
 models.append(emb_mod(word3))
-print(models)
+
+
 def simil(vec1, vec2):
   w = []
   for i in range(300):
     w.append(vec1[i] * vec2[i])
   return sum(w)
-simil(models[0], models[1])
+
+
 def pred_mod(us_words):
   us_vec = emb_mod(us_words)
   simils = []
@@ -49,6 +50,8 @@ def pred_mod(us_words):
     sim2model[sim] = model
     model += 1
   return sim2model[sim_max]
+
+
 
 #Ввод:
 #pred_mod(список прилагательных: ['уютный', 'модный'........])
